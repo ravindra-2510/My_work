e50d6dee90ac59088b8b55a25953857c83ebab21
Ravindra Yadav <65608632+ravindra-2510@users.noreply.github.com>
2021-04-18 07:08:46 +0530

    Create array_digonal.c

---
+++ array_digonal.c
@@ -0,0 +1,43 @@
+#include <stdio.h>  
+void main ()  
+{  
+       int num;
+       int arr[10][10],i,j;  
+      printf("enter the size of array (m*n): \n");
+      scanf("%d",&num);
+ //take element of array
+    for (i=0;i<num;i++)  
+    {  
+        for (j=0;j<num;j++)  
+        {  
+            scanf("%d",&arr[i][j]);  
+        }  
+    }  
+    //print array
+    printf("\n printing the elements ....\n");   
+    for(i=0;i<num;i++)  
+    {  
+        printf("\n");  
+        for (j=0;j<num;j++)  
+        {  
+            printf("%d\t",arr[i][j]);  
+        }  
+    }  
+   
+   //find digonal
+   
+    for(i=0;i<num;i++)  
+    {  
+        printf("\n");  
+        for (j=0;j<num;j++)  
+        {  
+           if(i==j)
+           {
+               printf("\ndigonal is:\n");
+            printf("\n %d\t",arr[i][j]); // print digonal
+           }
+        }  
+    }  
+   
+   
+}  
